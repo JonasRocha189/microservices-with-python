6288af4de328ed268610c65ef23f0d22cd742772
JonasRocha189 <jonas.rocha189@gmail.com>
2026-03-24 19:02:26 -0400

    book service finished

--- 2-microservice-ecomm/book/app.py
+++ 2-microservice-ecomm/book/app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from routes import book_blueprint
+from models import db, Book, init_app
+from flask_migrate import Migrate
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0Di-n1GgnMMa8OpdG3shdQ'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/book.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "database", "book.db")}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 app.register_blueprint(book_blueprint)
+init_app(app)
 
+migrate = Migrate(app, db)
 if __name__ == '__main__':
   app.run(debug=True, port=5502)
---
+++ 2-microservice-ecomm/book/models.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def init_app(app):
+  db.app = app
+  db.init_app(app)
+
+class Book(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(255), unique=True, nullable=False)
+  slug = db.Column(db.String(255), unique=True, nullable=False)
+  price = db.Column(db.Float, nullable=False)
+
+  def __repr__(self):
+    f'<Book {self.id}: {self.name}>'
+
+  def serialize(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'slug': self.slug,
+      'price': self.price
+    }
